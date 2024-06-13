a10886c3401f49fc1260274dbcf687489ff9cc1e
Bal Kishan <bkunsolved@gmail.com>
2024-06-13 17:41:04 +0530

    add something

--- utils/scrapper.js
+++ utils/scrapper.js
@@ -5,11 +5,11 @@ async function scrapeProduct(url) {
   const page = await browser.newPage();
   await page.goto(url);
 
-  const className = "._30jeq3" || "a-price-whole";
+  const className = ".Nx9bqj" || "a-price-whole";
 
   await page.waitForSelector(className);
 
-  const price = await page.$eval("._30jeq3", (element) => element.textContent);
+  const price = await page.$eval(".Nx9bqj", (element) => element.textContent);
 
   await browser.close();
 
